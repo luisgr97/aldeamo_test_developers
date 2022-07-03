d711826ff1002b614ad39481ee08f939ca895ee1
Luis Alberto Gómez <37640684+luisGomez231997@users.noreply.github.com>
2022-07-03 12:56:46 -0500

    feat: initial commit and project

---
+++ aldeamo_test/src/main/java/com/example/aldeamo_test/AldeamoTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.aldeamo_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AldeamoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AldeamoTestApplication.class, args);
+	}
+
+}
---
+++ aldeamo_test/src/test/java/com/example/aldeamo_test/AldeamoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aldeamo_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AldeamoTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
